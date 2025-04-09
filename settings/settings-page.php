d74f6ae43d1f25f38d767a5626f45905568d1594
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2025-04-09 09:48:32 +0200

    safer handling of data
        - sending body and title directly
        - checking for empty body
        - escaping body and title on post creation

--- settings/settings-page.php
+++ settings/settings-page.php
@@ -23,22 +23,27 @@ function create_api_post(WP_REST_Request $request){
 
     $body = json_decode($request->get_body(), true);
 
-    $postdata = [
-        'post_title' => $body['title'],
-        'post_content' => $body['body'],
-        'post_type' => 'api-data',
-        'post_status' => 'publish'
-  ];
-
-    $post_id = wp_insert_post($postdata);
-
-    if(gettype($post_id) == "integer") {
-
-        return new WP_Rest_Response('Post created successfully', 200);
-
-    } else {
-
-        return new WP_Rest_Response('Error creating post', 500);
+    if (!empty($body)){
+        $postdata = [
+            'post_title' => esc_html($body['title']),
+            'post_content' => esc_html($body['body']),
+            'post_type' => 'api-data',
+            'post_status' => 'publish'
+      ];
+    
+        $post_id = wp_insert_post($postdata);
+    
+        if(gettype($post_id) == "integer") {
+    
+            return new WP_Rest_Response('Post created successfully', 200);
+    
+        } else {
+    
+            return new WP_Rest_Response('Error creating post', 500);
+        }
+    }
+    else {
+        return new WP_Rest_Response('No post data found', 500);
     }
 
 }
@@ -62,13 +67,14 @@ function api_settings_page_view(){
 
  ?>
     <script>
-        const handlePost = async (element, id) => {
+        const handlePost = async (element, id, title, body) => {
             element.innerHTML = '<div class="spinner"></div>';
                 const res = await fetch("<?php echo get_rest_url(null, 'v1/api-posts/create');?>", {
                     method: "POST",
                     body: JSON.stringify({ 
-                        title: document.getElementById(`post-title-${id}`).innerHTML,
-                        body: document.getElementById(`post-body-${id}`).innerHTML,
+                        id: id,
+                        title: title,
+                        body: body,
                      }),
                     headers: {
                         "Content-Type": "application/json",
@@ -107,15 +113,15 @@ function api_settings_page_view(){
                         `<li style="display: flex; width: 100%; justify-content: space-between; ">
                         
                         <div style="display: flex; flex-direction: column; min-width: 12rem; max-width: 12rem; margin-right: 0.5rem">
-                            <p style="font-weight: bold">Title </p>
+                            <p style="font-weight: bold">Title</p>
                             <p id="post-title-${value.id}">${value.title}</p>
                         </div>
                         <div style="display: flex; flex-direction: column; width: 100%;">
-                            <p style="font-weight: bold">Body </p>
+                            <p style="font-weight: bold">Body</p>
                             <p id='post-body-${value.id}'>${value.body}</p>
                         </div>
                         <div style="display: flex; flex-direction: column; align-items: center; min-width: 4rem; max-width: 4rem; justify-content: center ">
-                            <button id="import" onclick="handlePost(this, ${value.id})">Import</button> 
+                            <button id="import" onclick="handlePost(this, ${value.id}, '${value.title}', '${value.body.replace(/[\n\r]+/g, " ")}')">Import</button> 
                         </div>
                         
                         </li>
